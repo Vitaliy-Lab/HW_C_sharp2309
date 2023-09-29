5ee2f9977c32b03cec4b0807304312c2f520a858
Vitaliy-lab <9175603000@mail.ru>
2023-09-29 18:20:52 +0300

    Добавил задачу в папку Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, 
+// 1. которая принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+// 456->5 
+// 782->8 
+// 918->1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number *= -1;
+}
+
+if (number >= 100 && number <= 999)
+{
+    int intermediateNumber = number / 10;
+    int secondNumber = intermediateNumber % 10;
+
+    Console.WriteLine($"Вторая цифра этого числа -> {secondNumber}");
+}
+else
+{
+    Console.WriteLine("Eror");
+}
