a2d38dede6647960cdd50247602e77e415574a12
Vitaliy-lab <9175603000@mail.ru>
2023-09-29 18:42:37 +0300

    Добавил задачу в папку Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу
+// 1. которая выводит третью цифру заданного числа или
+// 2. сообщает, что третьей цифры нет.
+// 645->5
+// 78 -> третьей цифры нет 
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+while (anyNumber > 999)
+{
+  anyNumber = anyNumber / 10;
+}
+if (anyNumber >= 100 && anyNumber < 999)
+{
+  anyNumber = anyNumber % 10;
+  Console.WriteLine($"третья цифра -> {anyNumber}");
+}
+else 
+{
+  Console.WriteLine("-> третьей цифры нет");
+}
